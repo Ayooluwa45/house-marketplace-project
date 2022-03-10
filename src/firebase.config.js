c22526f8fa37c6ed5b7eeee2013539993aed7a01
Emmanuel Sanni <sanniayooluwa45@gmail.com>
2022-03-10 16:55:46 +0100

    New updates

---
+++ src/firebase.config.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";import {getFirestore} from 'firebase/firestore'
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB6-Ju7nkLDfiEQuE9W-T2_HCdeCpAm5L8",
+  authDomain: "house-marketplace-app-f3c95.firebaseapp.com",
+  projectId: "house-marketplace-app-f3c95",
+  storageBucket: "house-marketplace-app-f3c95.appspot.com",
+  messagingSenderId: "741085995757",
+  appId: "1:741085995757:web:e6530187a15feec05de2e4"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const db= getFirestore()
