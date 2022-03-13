f92969893d4d9e76c684f326c478b50009637ee0
Emmanuel Sanni <sanniayooluwa45@gmail.com>
2022-03-13 11:55:07 +0100

    Explore page added

---
+++ src/pages/Explore.jsx
@@ -0,0 +1,19 @@
+import {Link} from 'react-router-dom'
+
+function Explore() {
+  return (
+    <div>
+    <Link to ='/sign-in'>
+      Sign In
+    </Link>
+    <Link to ='/sign-up'>
+      Sign In
+    </Link>
+    <Link to ='/sign-in'>
+      Sign In
+    </Link>
+    </div>
+  )
+}
+
+export default Explore
