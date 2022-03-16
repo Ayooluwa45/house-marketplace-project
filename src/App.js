3745311817c4d5bde83512196c41caae2fd6c025
Emmanuel Sanni <sanniayooluwa45@gmail.com>
2022-03-16 22:37:11 +0100

    New update

--- src/App.js
+++ src/App.js
@@ -1,29 +1,32 @@
-import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
-import {ToastContainer} from 'react-toastify'
-import 'react-toastify/dist/ReactToastify.css'
-import Navbar from './components/Navbar';
-import Explore from './pages/Explore';
-import Profile from './pages/Profile';
-import Offers from './pages/Offers';
-import SignIn from './pages/SignIn';
-import SignUp from './pages/SignUp';
-import ForgotPassword from './pages/ForgotPassword';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import Navbar from "./components/Navbar";
+import PrivateRoute from "./components/PrivateRoute";
+import Explore from "./pages/Explore";
+import Profile from "./pages/Profile";
+import Offers from "./pages/Offers";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import ForgotPassword from "./pages/ForgotPassword";
 
 function App() {
   return (
     <>
       <Router>
         <Routes>
-          <Route path='/' element={<Explore/>}/>
-          <Route path='/offers' element={<Offers/>}/>
-          <Route path='/profile' element={<Profile/>}/>
-          <Route path='/sign-in' element={<SignIn/>}/>
-          <Route path='/sign-up' element={<SignUp/>}/>
-          <Route path='/forgotpassword' element={<ForgotPassword/>}/>
+          <Route path="/" element={<Explore />} />
+          <Route path="/offers" element={<Offers />} />
+          <Route path="/profile" element={<PrivateRoute />}>
+            <Route path="/profile" element={<Profile />} />
+          </Route>
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/sign-up" element={<SignUp />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
         </Routes>
-        <Navbar/>
+        <Navbar />
       </Router>
-      <ToastContainer/>
+      <ToastContainer />
     </>
   );
 }
